e6925b401d488b77af81bf7cd9e74959ba1c324e
Zach Garwood <zachgarwood@gmail.com>
2015-11-28 13:44:36 -0600

    Added signin route
    ...for users to choose their authentication provider
    See #4

--- app/router.js
+++ app/router.js
@@ -2,13 +2,14 @@ import Ember from 'ember';
 import config from './config/environment';
 
 var Router = Ember.Router.extend({
-  location: config.locationType
+    location: config.locationType
 });
 
 Router.map(function() {
-  this.route('lists', {path: '/'});
-  this.route('list', {path: '/list/:list_id'});
-  this.route('item', {path: '/item/:item_id'});
+    this.route('lists', {path: '/'});
+    this.route('list', {path: '/list/:list_id'});
+    this.route('item', {path: '/item/:item_id'});
+    this.route('signin');
 });
 
 export default Router;
