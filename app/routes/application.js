2c370f1bf8197e3b2ac45a828c0b137c006dc43a
Zach Garwood <zachgarwood@gmail.com>
2015-11-28 13:45:23 -0600

    Transitioned to '/' on signin/signout
    See #4

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,19 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  beforeModel: function() {
-    return this.get("session").fetch().catch(function() {});
-  },
-
-  actions: {
-    signIn: function(provider) {
-      this.get("session").open("firebase", { provider: provider}).then(function(data) {
-        console.log(data.currentUser);
-      });
+    beforeModel: function() {
+        return this.get("session").fetch().catch(function() {});
     },
 
-    signOut: function() {
-      this.get("session").close();
+    actions: {
+        signIn: function(provider) {
+            let that = this;
+            this.get("session").open("firebase", {
+                provider: provider
+            }).then(function(data) {
+                that.transitionTo('/');
+            });
+        },
+
+        signOut: function() {
+            this.get("session").close();
+            this.transitionTo('/');
+        }
     }
-  }
 });
