77fd318ccf5e9d893fa6c74043444c1030e8eb16
Zach Garwood <zachgarwood@gmail.com>
2015-12-08 17:46:48 -0600

    Reformatted application route

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,23 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-    beforeModel: function() {
-        return this.get("session").fetch().catch(function() {});
+  beforeModel: function() {
+    return this.get('session').fetch().catch(function() {});
+  },
+
+  actions: {
+    signIn: function(provider) {
+      var route = this;
+      this.get('session').open('firebase', { provider: provider }).then(function() {
+        route.transitionTo('/');
+      }, function() {
+        route.transitionTo('/');
+      });
     },
 
-    actions: {
-        signIn: function(provider) {
-            var route = this;
-            this.get('session').open("firebase", { provider: provider }).then(function() {
-                route.transitionTo('/');
-            }, function() {
-                route.transitionTo('/');
-            });
-        },
-
-        signOut: function() {
-            this.get("session").close();
-            this.transitionTo('signin');
-        }
+    signOut: function() {
+      this.get('session').close();
+      this.transitionTo('signin');
     }
+  }
 });
