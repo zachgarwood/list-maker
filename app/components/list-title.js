7286826526a97029a8650ccfd049347feb0cdfcb
Zach Garwood <zachgarwood@gmail.com>
2016-01-26 19:46:34 -0600

    Updated list-title to hide count if < 2

--- app/components/list-title.js
+++ app/components/list-title.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  title: Ember.computed('list.items.[]', 'list.description', function() {
+    let count = this.get('list.items.length');
+    let title = this.get('list.description');
+    if (count > 1) {
+      title = `${count} ${title}`;
+    }
+
+    return title;
+  })
 });
